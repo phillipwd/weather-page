4ead9bcca0aa6aead09693cdebdac5e69bc012a9
Phillip Dunham <phillipwwd@gmail.com>
2019-12-25 15:52:31 -0800

    Button changes city variable, URL retrieves for that city.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 var key = "8da8c96f0f9e192e876aa314cfa3d9de"
 $(document).ready(function () {
-    var city = "seattle";
-    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
-
+    // var city = "seattle";
+    // var entry = $("#citySearch").val("Seattle");
+    
     function retrieve(){
+        var entry = $("#citySearch").val()
+        var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + entry + "&appid=" + key;
+        console.log(entry);
     $.get(queryUrl).then(function(response){
+        
         console.log(response);
         
     });
