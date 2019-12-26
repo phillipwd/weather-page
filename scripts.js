c08621bb5fc5682aba5e7b7b210fc0a57562552d
Phillip Dunham <phillipwwd@gmail.com>
2019-12-26 15:38:46 -0800

    building one-day div, searches appear in local storage.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 var key = "8da8c96f0f9e192e876aa314cfa3d9de"
 $(document).ready(function () {
-    // var city = "seattle";
-    // var entry = $("#citySearch").val("Seattle");
-    
+    // var saved = []
     function retrieve(){
         var entry = $("#citySearch").val()
+        localStorage.setItem(entry,entry);
         var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + entry + "&appid=" + key;
         console.log(entry);
     $.get(queryUrl).then(function(response){
-        
         console.log(response);
+        $(".cityName").text(response.name + response.weather[0].id);
+
         
     });
 }
